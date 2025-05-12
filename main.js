d3d0d4deefac2d5074d561da15dd876473a7a12b
harvest7777 <333harvest.moon333@gmail.com>
2025-05-11 20:21:10 -0700

    FEATURE: miat

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -10,19 +12,40 @@ const camera = new THREE.PerspectiveCamera(
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
+
+// renderer.setAnimationLoop(animate);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// le miata
+const loader = new GLTFLoader();
+const light = new THREE.AmbientLight(0xffffff, 1); // soft white light
+scene.add(light);
+
+const directionalLight = new THREE.DirectionalLight(light);
+directionalLight.position.set(5, 10, 7.5);
+scene.add(directionalLight);
+
+loader.load(
+  "miata.glb",
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 camera.position.z = 5;
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  requestAnimationFrame(animate);
+  // cube.rotation.x += 0.1;
+  // cube.rotation.y += 0.1;
+  controls.update();
   renderer.render(scene, camera);
 }
+animate();
